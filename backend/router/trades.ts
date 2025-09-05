6c3bbb6f14c373866ddb862163ec8f25046ff980
Hiperultimate <hiperultimatevegito@gmail.com>
2025-09-05 18:35:35 +0530

    init trade routes

---
+++ backend/router/trades.ts
@@ -0,0 +1,23 @@
+import express from "express";
+
+const tradeRouter = express.Router();
+
+tradeRouter.post("/trade/create", (req,res) => {
+
+})
+
+tradeRouter.post("/trade/close", (req,res) => {
+
+})
+
+tradeRouter.get("/balance/usd", (req,res) => {
+
+})
+
+tradeRouter.get("/balance", (req,res) => {
+
+})
+
+tradeRouter.get("/supportedAssets", (req,res) => {
+
+})
